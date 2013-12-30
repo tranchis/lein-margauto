7603088b460d01432ba7c3001448ade409f49964
Sergio Alvarez-Napagao <salvarez@lsi.upc.edu>
2013-12-30 17:38:38 +0100

    minimized project.clj options, preparing release 1.0.14

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject com.github.tranchis/lein-margauto "1.0.3"
+(defproject com.github.tranchis/lein-margauto "1.0.14"
   :description "Leiningen plugin: Autobuilder and simple server for Marginalia documentation."
   :plugins [[no-man-is-an-island/lein-eclipse "2.0.0"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [noir "1.3.0"]
-                 [leiningen "2.3.4"]
-                 [lein-marginalia "0.7.1"]])
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[lein-marginalia "0.7.1"]])
