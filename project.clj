d2a1189f8b1ec5967849d45cd24511c5dff5a73c
Sergio Alvarez-Napagao <salvarez@lsi.upc.edu>
2013-12-30 16:22:46 +0100

    reconfigured project.clj to use newer versions of leiningen and noir, configured project for eclipse project

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Leiningen plugin: Autobuilder and simple server for Marginalia documentation."
   :local-repo-classpath true
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
+  :plugins [[no-man-is-an-island/lein-eclipse "2.0.0"]]
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [noir "1.1.0-SNAPSHOT"]
-                 [leiningen "1.3.1"]
+                 [noir "1.3.0"]
+                 [leiningen "2.3.4"]
                  [lein-marginalia "0.6.0"]])
