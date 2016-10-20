33c3e2c9911a49d1515dfc470da6691d91209c64
Ron Lusk <rlusk@chariotsolutions.com>
2016-10-20 13:16:50 -0400

    Update to next SNAPSHOT
    Update the version of `marginalia`, and remove the `lein-eclipse` plugin

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject com.github.tranchis/lein-margauto "1.0.14"
+(defproject com.github.luskwater/lein-margauto "1.0.15-SNAPSHOT"
   :description "Leiningen plugin: Autobuilder and simple server for Marginalia documentation."
-  :plugins [[no-man-is-an-island/lein-eclipse "2.0.0"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[lein-marginalia "0.7.1"]])
+  :dependencies [[lein-marginalia "0.9.0"]]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                            :creds :gpg}]])
